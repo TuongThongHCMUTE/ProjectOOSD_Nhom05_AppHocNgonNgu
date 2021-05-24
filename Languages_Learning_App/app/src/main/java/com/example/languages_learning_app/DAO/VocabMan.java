294147a4d1d82fe03a23fa7ec90151130918d00f
dinhphu28 <18110175@student.hcmute.edu.vn>
2021-05-24 17:04:10 +0700

    Sth 1

---
+++ Languages_Learning_App/app/src/main/java/com/example/languages_learning_app/DAO/VocabMan.java
@@ -0,0 +1,60 @@
+package com.example.languages_learning_app.DAO;
+
+public class VocabMan {
+    private Integer level;
+    private Integer vid;
+    private String vword;
+    private String vspelling;
+    private String vmean;
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getVid() {
+        return vid;
+    }
+
+    public void setVid(Integer vid) {
+        this.vid = vid;
+    }
+
+    public String getVword() {
+        return vword;
+    }
+
+    public void setVword(String vword) {
+        this.vword = vword;
+    }
+
+    public String getVspelling() {
+        return vspelling;
+    }
+
+    public void setVspelling(String vspelling) {
+        this.vspelling = vspelling;
+    }
+
+    public String getVmean() {
+        return vmean;
+    }
+
+    public void setVmean(String vmean) {
+        this.vmean = vmean;
+    }
+
+    public VocabMan(Integer level, Integer vid, String vword, String vspelling, String vmean) {
+        this.level = level;
+        this.vid = vid;
+        this.vword = vword;
+        this.vspelling = vspelling;
+        this.vmean = vmean;
+    }
+
+    public VocabMan() {
+    }
+}
